af96526c6a19eb6b709120d239d313b16434f948
BlackSound1 <matthewr@videotron.ca>
2024-03-24 16:12:05 -0400

    Performance, code, and UX improvements

--- src/panes/memory.py
+++ src/panes/memory.py
@@ -1,115 +1,63 @@
 from psutil import virtual_memory
 from textual.app import ComposeResult
 from textual.containers import VerticalScroll
-from textual.css.query import NoMatches
 from textual.reactive import reactive
 from textual.widgets import Static
 
 from ..utilities import compute_percentage_color, bytes2human, COMMON_INTERVAL
 
 
+def _get_mem_data() -> dict:
+    """
+    Return a dictionary containing information about the memory usage.
+
+    :return: A dictionary with keys 'total', 'available', 'used', and 'percent'
+              representing total memory, available memory, used memory, and percentage used.
+    """
+    return {
+        "total": virtual_memory().total,
+        "available": virtual_memory().available,
+        "used": virtual_memory().used,
+        "percent": virtual_memory().percent
+    }
+
+
 class MemUsage(Static):
     BORDER_TITLE = "Memory Usage"
 
-    total = virtual_memory().total
-    available = reactive(0)
-    used = reactive(0)
-    percent = reactive(0.0)
+    mem_data = reactive(_get_mem_data())
 
-    def update_available(self) -> None:
+    def update_mem_data(self) -> None:
         """
-        Update the available memory by assigning the value of virtual_memory().available to self.available.
-        """
-        self.available = virtual_memory().available
-
-    def watch_available(self, avail: int) -> None:
-        """
-        Update memory information with the total, available, used memory, and percentage used.
-
-        :param avail: The updated amount of available memory in Bytes
-        :return: None
-        """
-
-        pct = compute_percentage_color(self.percent)
-        tot = bytes2human(self.total)
-        avail = bytes2human(avail)
-        used = bytes2human(self.used)
-        self.update(f"Total Memory: {tot}\nAvailable Memory: {avail}\nUsed: {used}\nPercentage Used: {pct}%")
-
-    def update_used(self) -> None:
-        """
-        Update the used memory by assigning the value of virtual_memory().used to self.used.
+        Update the memory information by calling `_get_mem_data`
 
         :return: None
         """
-        self.used = virtual_memory().used
+        self.mem_data = _get_mem_data()
 
-    def watch_used(self, u: int) -> None:
+    def watch_mem_data(self, data: dict) -> None:
         """
-        Update memory information with the total, available, used memory, and percentage used.
+        Watch for changes in the memory data and update the Static widget accordingly
 
-        :param u: The updated amount of used memory in Bytes
+        :param data: The new memory data
         :return: None
         """
 
-        # Compute the percentage used and format the memory values
-        pct = compute_percentage_color(self.percent)
+        static = self.query_one("Static", Static)
 
-        # Convert memory sizes to human-readable format
-        tot = bytes2human(self.total)
-        avail = bytes2human(self.available)
-        used = bytes2human(u)
-
-        # Update the memory information in the UI
-        try:
-            (
-                self.query_one("#mem_static", expect_type=Static)
-                .update(f"Total Memory: {tot}\nAvailable Memory: {avail}\nUsed: {used}\nPercentage Used: {pct}%")
-            )
-        except NoMatches:
-            pass
-
-    def update_percent(self) -> None:
-        """
-        Update the percentage used by assigning the value of virtual_memory().percent to self.percent
-
-        :return: None
-        """
-        self.percent = virtual_memory().percent
-
-    def watch_percent(self, pct: float) -> None:
-        """
-        Updates the memory information with the total, available, used memory, and percentage used
-
-        :param pct: The updated percentage used
-        :return: None
-        """
-
-        # Compute the color based on the percentage
-        pct = compute_percentage_color(pct)
-
-        # Convert memory sizes to human-readable format
-        tot = bytes2human(self.total)
-        avail = bytes2human(self.available)
-        used = bytes2human(self.used)
-
-        # Update the memory information in the UI
-        try:
-            (
-                self.query_one("#mem_static", expect_type=Static)
-                .update(f"Total Memory: {tot}\nAvailable Memory: {avail}\nUsed: {used}\nPercentage Used: {pct}%")
-            )
-        except NoMatches:
-            pass
+        static.update(f"Total Memory: {bytes2human(data['total'])}\n\n"
+                      f"Available Memory: {bytes2human(data['available'])}\n\n"
+                      f"Used: {bytes2human(data['used'])}\n\n"
+                      f"Percentage Used: {compute_percentage_color(data['percent'])}%")
 
     def compose(self) -> ComposeResult:
         """
-        Generate a ComposeResult by yielding a Static widget with the memory information.
+        Generate a ComposeResult by yielding a vertically-scrolling Static widget with the memory information.
 
         :return: The ComposeResult
         """
         with VerticalScroll():
-            yield Static(id="mem_static")
+            yield Static(id="mem-static")
 
     def on_mount(self) -> None:
         """
@@ -117,6 +65,4 @@ class MemUsage(Static):
 
         :return: None
         """
-        self.update_available = self.set_interval(COMMON_INTERVAL, self.update_available)
-        self.update_percent = self.set_interval(COMMON_INTERVAL, self.update_percent)
-        self.update_used = self.set_interval(COMMON_INTERVAL, self.update_used)
+        self.update_mem_data = self.set_interval(COMMON_INTERVAL, self.update_mem_data)
