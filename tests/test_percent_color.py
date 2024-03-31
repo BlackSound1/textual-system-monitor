f25fa9631ff5de8adf563b33a43770ad29e642b9
BlackSound1 <matthewr@videotron.ca>
2024-03-30 21:51:14 -0400

    Fixed tests

--- tests/test_percent_color.py
+++ tests/test_percent_color.py
@@ -6,27 +6,27 @@ from src.utilities import compute_percentage_color
 class TestPercentColor(unittest.TestCase):
     def test_percent_green(self):
         string = compute_percentage_color(1)
-        self.assertIn('[#70f97E]', string)
+        self.assertIn('[green]', string)
         self.assertIn('[/]', string)
 
     def test_percent_yellow(self):
         string = compute_percentage_color(80)
-        self.assertIn('[#F9F070]', string)
+        self.assertIn('[yellow]', string)
         self.assertIn('[/]', string)
 
     def test_percent_red(self):
         string = compute_percentage_color(95)
-        self.assertIn('[#F76A5D]', string)
+        self.assertIn('[red]', string)
         self.assertIn('[/]', string)
 
     def test_too_low(self):
         string = compute_percentage_color(-100)
-        self.assertIn('[#70f97E]', string)
+        self.assertIn('[green]', string)
         self.assertIn('[/]', string)
 
     def test_too_high(self):
         string = compute_percentage_color(200)
-        self.assertIn('[#F76A5D]', string)
+        self.assertIn('[red]', string)
         self.assertIn('[/]', string)
 
 
