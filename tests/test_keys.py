b943efd34045654944aba8b5c3ec27d00bd61c4b
BlackSound1 <matthewr@videotron.ca>
2024-04-04 15:22:24 -0400

    Refactored tests to use class variable for Monitor

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -1,7 +1,6 @@
 from unittest import IsolatedAsyncioTestCase
 
 import pytest
-import pytest_asyncio
 
 from src.app import Monitor
 from src.screens.cpu_screen import CPU_Screen
@@ -12,100 +11,92 @@ from src.screens.network_screen import NetworkScreen
 from src.screens.processes_screen import ProcessesScreen
 
 
-# @pytest_asyncio.fixture()
-# def monitor_app_fixture():
-#     monitor_app = Monitor()
-#     yield monitor_app
-
-
 class TestKeys(IsolatedAsyncioTestCase):
 
-    # @classmethod
-    # async def asyncSetUp(cls):
-    #     monitor_app = Monitor()
-    #     cls
+    @classmethod
+    async def asyncSetUp(cls):
+        cls.monitor_app = Monitor()
 
     @pytest.mark.asyncio
     async def test_process_screen(self):
-        monitor_app = Monitor()
 
-        async with monitor_app.run_test() as pilot:
+        async with self.monitor_app.run_test() as pilot:
             await pilot.press("p")
 
-            assert type(monitor_app.screen) is ProcessesScreen
+            assert type(self.monitor_app.screen) is ProcessesScreen
 
-            assert monitor_app.screen.BORDER_TITLE == "Processes"
+            assert self.monitor_app.screen.BORDER_TITLE == "Processes"
 
             await pilot.press("p")
 
-            assert type(monitor_app.screen) is MainScreen
+            assert type(self.monitor_app.screen) is MainScreen
 
-            assert monitor_app.screen.BORDER_TITLE == ""
+            assert self.monitor_app.screen.BORDER_TITLE == ""
 
     @pytest.mark.asyncio
     async def test_drives_screen(self):
-        monitor_app = Monitor()
 
-        async with monitor_app.run_test() as pilot:
+        async with self.monitor_app.run_test() as pilot:
             await pilot.press("d")
 
-            assert type(monitor_app.screen) is DriveScreen
+            assert type(self.monitor_app.screen) is DriveScreen
 
-            assert monitor_app.screen.BORDER_TITLE == "Drive Usage"
+            assert self.monitor_app.screen.BORDER_TITLE == "Drive Usage"
 
             await pilot.press("d")
 
-            assert type(monitor_app.screen) is MainScreen
+            assert type(self.monitor_app.screen) is MainScreen
 
-            assert monitor_app.screen.BORDER_TITLE == ""
+            assert self.monitor_app.screen.BORDER_TITLE == ""
 
     @pytest.mark.asyncio
     async def test_memory_screen(self):
-        monitor_app = Monitor()
 
-        async with monitor_app.run_test() as pilot:
+        async with self.monitor_app.run_test() as pilot:
             await pilot.press("m")
 
-            assert type(monitor_app.screen) is MemoryScreen
+            assert type(self.monitor_app.screen) is MemoryScreen
 
-            assert monitor_app.screen.BORDER_TITLE == "Memory"
+            assert self.monitor_app.screen.BORDER_TITLE == "Memory"
 
             await pilot.press("m")
 
-            assert type(monitor_app.screen) is MainScreen
+            assert type(self.monitor_app.screen) is MainScreen
 
-            assert monitor_app.screen.BORDER_TITLE == ""
+            assert self.monitor_app.screen.BORDER_TITLE == ""
 
     @pytest.mark.asyncio
     async def test_cpu_screen(self):
-        monitor_app = Monitor()
 
-        async with monitor_app.run_test() as pilot:
+        async with self.monitor_app.run_test() as pilot:
             await pilot.press("c")
 
-            assert type(monitor_app.screen) is CPU_Screen
+            assert type(self.monitor_app.screen) is CPU_Screen
 
-            assert monitor_app.screen.BORDER_TITLE == "CPU Usage"
+            assert self.monitor_app.screen.BORDER_TITLE == "CPU Usage"
 
             await pilot.press("c")
 
-            assert type(monitor_app.screen) is MainScreen
+            assert type(self.monitor_app.screen) is MainScreen
 
-            assert monitor_app.screen.BORDER_TITLE == ""
+            assert self.monitor_app.screen.BORDER_TITLE == ""
 
     @pytest.mark.asyncio
     async def test_network_screen(self):
-        monitor_app = Monitor()
 
-        async with monitor_app.run_test() as pilot:
+        async with self.monitor_app.run_test() as pilot:
             await pilot.press("n")
 
-            assert type(monitor_app.screen) is NetworkScreen
+            assert type(self.monitor_app.screen) is NetworkScreen
 
-            assert monitor_app.screen.BORDER_TITLE == "Network"
+            assert self.monitor_app.screen.BORDER_TITLE == "Network"
 
             await pilot.press("n")
 
             assert type(monitor_app.screen) is MainScreen
+            assert type(self.monitor_app.screen) is MainScreen
+
+            assert self.monitor_app.screen.BORDER_TITLE == ""
+
 
             assert monitor_app.screen.BORDER_TITLE == ""
