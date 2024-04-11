4a7aa8e6ee316a48c1ca728215b8235d4ede46bc
BlackSound1 <matthewr@videotron.ca>
2024-04-11 19:13:27 -0400

    Working avail Memory Screen info

--- src/screens/mem_screen.py
+++ src/screens/mem_screen.py
@@ -51,24 +51,28 @@ class MemoryScreen(Screen):
 
         try:
             total = self.query_one("#total-static", Static)
-            avail = self.query_one("#avail-static", Static)
+            avail = self.query_one("#avail-digits", Digits)
+            avail_label = self.query_one("#avail-static-label", Static)
             used = self.query_one("#used-digits", Digits)
             used_label = self.query_one("#used-static-label", Static)
             perc = self.query_one("#perc-digits", Digits)
         except NoMatches:
             return
 
-        pct, color = compute_percentage_color(data['percent'], combine_output=False)
-
         total.update(f"Total Memory: {bytes2human(data['total'])}")
-        avail.update(f"Available Memory: {bytes2human(data['available'])}")
+
+        # Updated available information
+        value, denom = bytes2human(data['available']).split(' ')
+        avail_label.update(f"Available Memory ({denom}):\t")
+        avail.update(f"{value}")
 
         # Update used information
-        value, denom = bytes2human(data['total']).split(' ')
-        used_label.update(f"Used ({denom}):\t")
+        value, denom = bytes2human(data['used']).split(' ')
+        used_label.update(f"Used Memory ({denom}):\t")
         used.update(f"{value}")
 
         # Update percentage information
+        pct, color = compute_percentage_color(data['percent'], combine_output=False)
         perc = reset_percentage_color(perc)
         perc.add_class(color)
         perc.update(f"{pct}")
@@ -83,7 +87,9 @@ class MemoryScreen(Screen):
         yield Header(show_clock=True)
         with Container(id="mem-container"):
             yield Static(id="total-static", classes="mem-static")
-            yield Static(id="avail-static", classes="mem-static")
+            with Container(classes="mem-static"):
+                yield Static("", id="avail-static-label", classes="label")
+                yield Digits(id="avail-digits", classes="digits")
             with Container(classes="mem-static"):
                 yield Static("", id="used-static-label", classes="label")
                 yield Digits(id="used-digits", classes="digits")
