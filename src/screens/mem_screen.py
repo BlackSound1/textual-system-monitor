05fcd84b9d67fb94f5d49a3b146713e0c3489c07
BlackSound1 <matthewr@videotron.ca>
2024-04-11 19:07:53 -0400

    Working used and percentage Memory Screen info

--- src/screens/mem_screen.py
+++ src/screens/mem_screen.py
@@ -9,6 +9,21 @@ from textual.widgets import Static, Header, Footer, Digits
 from src.utilities import get_mem_data, bytes2human, compute_percentage_color, COMMON_INTERVAL
 
 
+def reset_percentage_color(digits: Digits) -> Digits:
+    """
+    Reset the percentage color
+
+    :param digits: The Digits widget
+    :return: None
+    """
+
+    digits.remove_class("green")
+    digits.remove_class("yellow")
+    digits.remove_class("red")
+
+    return digits
+
+
 class MemoryScreen(Screen):
     BORDER_TITLE = "Memory"
     CSS_PATH = "../styles/mem_css.tcss"
@@ -37,7 +52,8 @@ class MemoryScreen(Screen):
         try:
             total = self.query_one("#total-static", Static)
             avail = self.query_one("#avail-static", Static)
-            used = self.query_one("#used-static", Static)
+            used = self.query_one("#used-digits", Digits)
+            used_label = self.query_one("#used-static-label", Static)
             perc = self.query_one("#perc-digits", Digits)
         except NoMatches:
             return
@@ -46,8 +62,15 @@ class MemoryScreen(Screen):
 
         total.update(f"Total Memory: {bytes2human(data['total'])}")
         avail.update(f"Available Memory: {bytes2human(data['available'])}")
-        used.update(f"Used: {bytes2human(data['used'])}")
-        perc.set_classes(color)
+
+        # Update used information
+        value, denom = bytes2human(data['total']).split(' ')
+        used_label.update(f"Used ({denom}):\t")
+        used.update(f"{value}")
+
+        # Update percentage information
+        perc = reset_percentage_color(perc)
+        perc.add_class(color)
         perc.update(f"{pct}")
 
     def compose(self) -> ComposeResult:
@@ -61,10 +84,12 @@ class MemoryScreen(Screen):
         with Container(id="mem-container"):
             yield Static(id="total-static", classes="mem-static")
             yield Static(id="avail-static", classes="mem-static")
-            yield Static(id="used-static", classes="mem-static")
             with Container(classes="mem-static"):
-                yield Static("Percentage Used (%):\t", id="used-static-label")
-                yield Digits(id="perc-digits")
+                yield Static("", id="used-static-label", classes="label")
+                yield Digits(id="used-digits", classes="digits")
+            with Container(classes="mem-static"):
+                yield Static("Percentage Used (%):\t", classes="label")
+                yield Digits(id="perc-digits", classes="digits")
         yield Footer()
 
     def on_mount(self) -> None:
