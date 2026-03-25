8a430eb7017961a4197ae2ad9dd3f63635a34979
BlackSound1 <matthewr@videotron.ca>
2026-03-24 20:57:48 -0400

    Fixed coloring test

--- tests/test_percent_color.py
+++ tests/test_percent_color.py
@@ -5,30 +5,24 @@ from src.utilities import compute_percentage_color
 
 class TestPercentColor(unittest.TestCase):
     def test_percent_green(self):
-        string = compute_percentage_color(1)
-        self.assertIn('[green]', string)
-        self.assertIn('[/]', string)
+        _, color = compute_percentage_color(1)
+        self.assertEqual(color, "green")
 
     def test_percent_yellow(self):
-        string = compute_percentage_color(80)
-        self.assertIn('[yellow]', string)
-        self.assertIn('[/]', string)
+        _, color = compute_percentage_color(80)
+        self.assertEqual(color, "yellow")
 
     def test_percent_red(self):
-        string = compute_percentage_color(95)
-        self.assertIn('[red]', string)
-        self.assertIn('[/]', string)
+        _, color = compute_percentage_color(95)
+        self.assertEqual(color, "red")
 
     def test_too_low(self):
-        string = compute_percentage_color(-100)
-        self.assertIn('[green]', string)
-        self.assertIn('[/]', string)
+        _, color = compute_percentage_color(-100)
+        self.assertEqual(color, "green")
 
     def test_too_high(self):
-        string = compute_percentage_color(200)
-        self.assertIn('[red]', string)
-        self.assertIn('[/]', string)
-
+        _, color = compute_percentage_color(200)
+        self.assertEqual(color, "red")
 
 if __name__ == '__main__':
     unittest.main()
