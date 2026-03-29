87595dde49ddfd2b68b1f4c6d8ad349ae7308711
BlackSound1 <matthewr@videotron.ca>
2026-03-29 16:04:31 -0400

    Added KB-related tests

--- tests/test_bytes.py
+++ tests/test_bytes.py
@@ -13,24 +13,32 @@ async def test_neg_bytes() -> None:
 
 async def test_KB() -> None:
     assert bytes_to_human(1_024) == "1.0 KiB"
+    assert bytes_to_human(1_000, base=1000) == "1.0 KB"
 
 async def test_MB() -> None:
     assert bytes_to_human(1_048_576) == "1.0 MiB"
+    assert bytes_to_human(1_000_000, base=1000) == "1.0 MB"
 
 async def test_GB() -> None:
     assert bytes_to_human(1_073_741_824) == "1.0 GiB"
+    assert bytes_to_human(1_000_000_000, base=1000) == "1.0 GB"
 
 async def test_TB() -> None:
     assert bytes_to_human(1_099_511_627_776) == "1.0 TiB"
+    assert bytes_to_human(1_000_000_000_000, base=1000) == "1.0 TB"
 
 async def test_PB() -> None:
     assert bytes_to_human(1_125_899_906_842_624) == "1.0 PiB"
+    assert bytes_to_human(1_000_000_000_000_000, base=1000) == "1.0 PB"
 
 async def test_EB() -> None:
     assert bytes_to_human(1_152_921_504_606_846_976) == "1.0 EiB"
+    assert bytes_to_human(1_000_000_000_000_000_000, base=1000) == "1.0 EB"
 
 async def test_ZB() -> None:
     assert bytes_to_human(1_180_591_620_717_411_303_424) == "1.0 ZiB"
+    assert bytes_to_human(1_000_000_000_000_000_000_000, base=1000) == "1.0 ZB"
 
 async def test_YB() -> None:
     assert bytes_to_human(1_208_925_819_614_629_174_706_176) == "1.0 YiB"
+    assert bytes_to_human(1_000_000_000_000_000_000_000_000, base=1000) == "1.0 YB"
