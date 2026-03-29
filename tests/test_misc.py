6fcec547a59f8b68014114494a431379eb9efe5b
BlackSound1 <matthewr@videotron.ca>
2026-03-29 16:44:39 -0400

    Tested Monitor.run()

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,4 +1,7 @@
+from unittest.mock import patch
+
 from src.app import Monitor
+from main import run
 
 
 async def test_change_KB_base() -> None:
@@ -11,3 +14,12 @@ async def test_change_KB_base() -> None:
     assert app.CONTEXT['kb_size'] == 1000
     app.action_switch_base()
     assert app.CONTEXT['kb_size'] == 1024
+
+
+async def test_run_calls_monitor_run() -> None:
+    """
+    Test that when the app starts, the `run()` method calls `Monitor.run()`
+    """
+    with patch('src.app.Monitor.run') as mock_monitor_run:
+        run()
+        mock_monitor_run.assert_called_once()
