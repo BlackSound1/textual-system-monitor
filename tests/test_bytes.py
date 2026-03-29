582f08dde984cd10177cd0d990380421f3395eb8
BlackSound1 <matthewr@videotron.ca>
2026-03-28 20:14:57 -0400

    Refactor bytes tests into new style. Add new text-only option

--- tests/test_bytes.py
+++ tests/test_bytes.py
@@ -1,55 +1,49 @@
-import unittest
+import pytest
 
 from src.utilities import bytes_to_human
 
 
-class TestBytes(unittest.TestCase):
-    def test_documentation_examples(self):
-        first_val = bytes_to_human(10_000)
-        second_val = bytes_to_human(100_001_221)
-        self.assertEqual(first_val, '9.8 KiB')
-        self.assertEqual(second_val, '95.4 MiB')
+@pytest.mark.asyncio
+async def test_documentation_examples() -> None:
+    assert bytes_to_human(10_000) == '9.8 KiB'
+    assert bytes_to_human(100_001_221) == '95.4 MiB'
 
-    def test_bytes_only(self):
-        val = bytes_to_human(100)
-        self.assertEqual(val, '100.0 B')
+@pytest.mark.asyncio
+async def test_bytes_only() -> None:
+    assert bytes_to_human(100) == '100.0 B'
 
-    def test_neg_bytes(self):
-        val = bytes_to_human(-1)
-        self.assertEqual(val, "0.0 B")
+@pytest.mark.asyncio
+async def test_neg_bytes() -> None:
+    assert bytes_to_human(-1) == "0.0 B"
 
-    def test_KB(self):
-        val = bytes_to_human(1_024)
-        self.assertEqual(val, "1.0 KiB")
+@pytest.mark.asyncio
+async def test_KB() -> None:
+    assert bytes_to_human(1_024) == "1.0 KiB"
 
-    def test_MB(self):
-        val = bytes_to_human(1_048_576)
-        self.assertEqual(val, "1.0 MiB")
+@pytest.mark.asyncio
+async def test_MB() -> None:
+    assert bytes_to_human(1_048_576) == "1.0 MiB"
 
-    def test_GB(self):
-        val = bytes_to_human(1_073_741_824)
-        self.assertEqual(val, "1.0 GiB")
+@pytest.mark.asyncio
+async def test_GB() -> None:
+    assert bytes_to_human(1_073_741_824) == "1.0 GiB"
 
-    def test_TB(self):
-        val = bytes_to_human(1_099_511_627_776)
-        self.assertEqual(val, "1.0 TiB")
+@pytest.mark.asyncio
+async def test_TB() -> None:
+    assert bytes_to_human(1_099_511_627_776) == "1.0 TiB"
 
-    def test_PB(self):
-        val = bytes_to_human(1_125_899_906_842_624)
-        self.assertEqual(val, "1.0 PiB")
+@pytest.mark.asyncio
+async def test_PB() -> None:
+    assert bytes_to_human(1_125_899_906_842_624) == "1.0 PiB"
 
-    def test_EB(self):
-        val = bytes_to_human(1_152_921_504_606_846_976)
-        self.assertEqual(val, "1.0 EiB")
+@pytest.mark.asyncio
+async def test_EB() -> None:
+    assert bytes_to_human(1_152_921_504_606_846_976) == "1.0 EiB"
 
-    def test_ZB(self):
-        val = bytes_to_human(1_180_591_620_717_411_303_424)
-        self.assertEqual(val, "1.0 ZiB")
+@pytest.mark.asyncio
+async def test_ZB() -> None:
+    assert bytes_to_human(1_180_591_620_717_411_303_424) == "1.0 ZiB"
 
-    def test_YB(self):
-        val = bytes_to_human(1_208_925_819_614_629_174_706_176)
-        self.assertEqual(val, "1.0 YiB")
-
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.asyncio
+async def test_YB() -> None:
+    assert bytes_to_human(1_208_925_819_614_629_174_706_176) == "1.0 YiB"
