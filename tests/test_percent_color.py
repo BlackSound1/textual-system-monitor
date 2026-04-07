30fd862e07e26bd47f6aa56f9c98d4702b68615c
BlackSound1 <matthewr@videotron.ca>
2026-04-07 17:18:34 -0400

    Reformatted some tests

--- tests/test_percent_color.py
+++ tests/test_percent_color.py
@@ -1,20 +1,25 @@
 from src.utilities import compute_percentage_color
 
+
 async def test_percent_green():
     _, color = compute_percentage_color(1)
     assert color == "green"
+
 
 async def test_percent_yellow():
     _, color = compute_percentage_color(80)
     assert color == "yellow"
 
+
 async def test_percent_red():
     _, color = compute_percentage_color(95)
     assert color == "red"
 
+
 async def test_too_low():
     _, color = compute_percentage_color(-100)
     assert color == "green"
+
 
 async def test_too_high():
     _, color = compute_percentage_color(200)
